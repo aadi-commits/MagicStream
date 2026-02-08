520be5f755d51ed78517b39e13a8b6f5a5fb19cb
Adit <aditnavle@gmail.com>
2026-02-08 11:20:24 +0530

    basic test controller for movies list

--- Server/MagicStreamServer/main.go
+++ Server/MagicStreamServer/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	controller "github.com/aadi-commits/MagicStream/Server/MagicStreamServer/controllers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +14,8 @@ func main(){
 	router.GET("/hello", func(c *gin.Context){
 		c.String(200, "Hello, Magic Stream Movies!")
 	})
+
+	router.GET("/movies", controller.GetMovies())
 
 	if err:= router.Run(":8080"); err!= nil{
 		fmt.Println("Failed to start server", err)
